420e3e288f5748beefcb2c32c246187f3e69c950
geoffrey woodcock <theatomproject@gmail.com>
2018-11-02 17:54:29 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,20 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+  
+  def self.sailors
+    includes(boats: :classifications).where("classifications.name = ?", "Sailboat").uniq
+  end
+  
+  def self.talented_seafarers
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboats.pluck(:id))
+  end
+  
+  def self.non_sailors
+    
+  end
+  
 end
